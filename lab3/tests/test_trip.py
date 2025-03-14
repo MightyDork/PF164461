0a8a2e77524a8285a7a1fccb6316921a724eaa56
UWM_PC <ewelina.dolega.02@gmail.com>
2025-03-14 08:55:00 +0100

    lab3 cz5

--- lab3/tests/test_trip.py
+++ lab3/tests/test_trip.py
@@ -17,6 +17,11 @@ class TestTrip(unittest.TestCase):
         trip2 = Trip("Rome", 5)
         self.assertEqual(trip2.calculate_cost(),500)
 
+    def test_add_participant(self):
+        trip1 = Trip("Paris", 7)
+        trip1.add_participant("John")
+        self.assertIn(self, "John", trip1.participants)
+
     def tearDown(self):
         pass
 
