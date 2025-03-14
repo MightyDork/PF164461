f978bbdf5218fb54a50d37337e9baca47d72b5ec
UWM_PC <ewelina.dolega.02@gmail.com>
2025-03-14 09:12:52 +0100

    lab3 cz6

--- lab3/src/trip.py
+++ lab3/src/trip.py
@@ -1,8 +1,12 @@
 class Trip:
 
     def __init__(self,destination,duration):
+        self.participants = []
         self.destination = destination
         self.duration = duration
 
     def calculate_cost(self):
         return self.duration * 100
+
+    def add_participant(self, participant):
+        self.participants.append(participant)
